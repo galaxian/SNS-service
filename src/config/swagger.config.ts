aae5699f79e602316a0653f84b9135aaecac5dd1
Lee <94841127+galaxian@users.noreply.github.com>
2022-09-23 14:08:06 +0900

    feat : swagger config

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,23 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('SNS-service API docs')
+    .setDescription('SNS-service API description')
+    .setVersion('1.0.0')
+    .addTag('api')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        name: 'JWT',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+}
