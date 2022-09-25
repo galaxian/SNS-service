1d54ddb56648182129bec4feaf54abbc8edb977f
Lee <94841127+galaxian@users.noreply.github.com>
2022-09-25 21:05:20 +0900

    feat : enity 연관관계 설정

--- src/board/entity/board.entity.ts
+++ src/board/entity/board.entity.ts
@@ -1,6 +1,13 @@
 import { Thumb } from 'src/thumb/entity/thumb.entity';
+import { User } from 'src/user/entity/user.entity';
 import { TimeStampableEntity } from 'src/util/entity/timeStamp.entity';
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Board extends TimeStampableEntity {
@@ -18,4 +25,7 @@ export class Board extends TimeStampableEntity {
 
   @OneToMany(() => Thumb, (thumb) => thumb.board, { eager: false })
   thumb: Thumb[];
+
+  @ManyToOne(() => User, (user) => user.board, { eager: false })
+  user: User;
 }
