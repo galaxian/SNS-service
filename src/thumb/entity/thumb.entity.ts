4bdf3fdb35a0b6711a342cae7d309e3b5ca084fe
Lee <94841127+galaxian@users.noreply.github.com>
2022-09-26 17:20:47 +0900

    feat(thumb) : thumb entity 좋아요 column boolean 추가

--- src/thumb/entity/thumb.entity.ts
+++ src/thumb/entity/thumb.entity.ts
@@ -1,7 +1,7 @@
 import { Board } from 'src/board/entity/board.entity';
 import { User } from 'src/user/entity/user.entity';
 import { TimeStampableEntity } from 'src/util/entity/timeStamp.entity';
-import { Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Thumb extends TimeStampableEntity {
@@ -13,4 +13,7 @@ export class Thumb extends TimeStampableEntity {
 
   @ManyToOne(() => Board, (board) => board.thumb, { eager: false })
   board: Board;
+
+  @Column({ default: true })
+  isThumb: boolean;
 }
